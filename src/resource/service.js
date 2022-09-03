2b372a8998aad66d3b95284f72e06fe77e6ef492
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-03 15:13:50 +0800

    feat: resource api return root&&name

--- src/resource/service.js
+++ src/resource/service.js
@@ -6,15 +6,19 @@ const isDirectory = p => fs.lstatSync(p).isDirectory();
 
 const walk = dir => {
     const f = fs.readdirSync(dir).map(name => path.join(dir, name));
-    return f.map(value => {
-        console.log(`f is ${value}`);
-        if (isDirectory(value)) {
-            const children = walk(value);
-            return new Resource(value, children);
-        } else {
-            return new Resource(value);
-        }
-    });
+    return {
+        name: dir,
+        root: dir,
+        content: f.map(value => {
+            console.log(`f is ${value}`);
+            if (isDirectory(value)) {
+                const children = walk(value);
+                return new Resource(value, children);
+            } else {
+                return new Resource(value);
+            }
+        }),
+    };
 };
 
 module.exports = walk;

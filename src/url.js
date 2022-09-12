375ad0c7ec3df2e014d3287b7aba4b118c55124c
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-12 21:38:45 +0800

    fix: 回滚之前的 url 拼接做法
    暂时先这么干，没想到 windows 上的 node.path.join() 和浏览器 url 不同

--- src/url.js
+++ src/url.js
@@ -1,8 +1,19 @@
 const R = require('ramda')
 
-module.exports = R.pipe(
-    R.apply(require('path').join),
-    // 根据之前实现的偏好 掐头去尾
-    // 但其实是否必要 有待商榷
-    R.replace(/^\/|\/$/g, ''),
-)
+const removeStart = (ch) => (str) => {
+    if (str.startsWith(ch)) {
+        return str.slice(ch.length)
+    }
+    return str
+}
+
+const removeEnd = (ch) => (str) => {
+    if (str.endsWith(ch)) {
+        return str.slice(0, -1 * ch.length)
+    }
+    return str
+}
+
+const handle = R.compose(removeStart('/'), removeEnd('/'))
+
+module.exports = R.pipe(R.reject(R.isEmpty), R.map(handle), R.join('/'))
